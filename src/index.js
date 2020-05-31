4742c5f5ce1818fb83731dfb6624b6ea57c4635b
Luong Minh Duc <duclm.techmaster@gmail.com>
2020-05-31 22:20:07 +0700

    Enable redux-thunk

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import { Provider } from 'react-redux'
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import rootReducer from "./redux/reducers"
+import thunk from 'redux-thunk'
 
 import './index.css';
 
@@ -12,7 +13,10 @@ import * as serviceWorker from './serviceWorker';
 
 const store = createStore(
   rootReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  compose(
+    applyMiddleware(thunk),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
 )
 
 ReactDOM.render(
