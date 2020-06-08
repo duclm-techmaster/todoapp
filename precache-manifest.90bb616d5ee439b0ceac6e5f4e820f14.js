9bd2832dfc7fbb7be61c3bfe556a2baa02b9f88f
Luong Minh Duc <duclm.techmaster@gmail.com>
2020-06-08 15:12:18 +0700

    Updates

---
+++ precache-manifest.90bb616d5ee439b0ceac6e5f4e820f14.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f55ce76e9149588cc4b954f3423e629",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e0916d3048ce4654bd6d",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "8eb286b94659e9cb2cd5",
+    "url": "/static/js/2.7685e108.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7685e108.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0916d3048ce4654bd6d",
+    "url": "/static/js/main.452ab9ca.chunk.js"
+  },
+  {
+    "revision": "c64dd9dd796352438c55",
+    "url": "/static/js/runtime-main.4db15ca9.js"
+  }
+]);
